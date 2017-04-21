dbfd59629cdf97043dc180c2970475a9c65d5d07
電車君 <densyakun1128@gmail.com>
2017-04-21 09:10:54 +0900

    no message

--- src/main/java/net/densyakun/trainsim/AccidentCause.java
+++
@@ -1,7 +0,0 @@
-package net.densyakun.trainsim;
-public enum AccidentCause {
-	noaccident, derailment, crush, unknown;
-	public boolean isBroken() {
-		return this == derailment || this == crush;
-	}
-}
--- src/main/java/net/densyakun/trainsim/MasterController.java
+++
@@ -1,9 +0,0 @@
-package net.densyakun.trainsim;
-
-//マスコン
-public abstract class MasterController {
-	public void setPower(Train train, double accel, double brake) {
-		train.setAccel(accel < 0 ? 0 : accel > 1 ? 1 : accel);
-		train.setBrake(brake < 0 ? 0 : brake > 1 ? 1 : brake);
-	}
-}
